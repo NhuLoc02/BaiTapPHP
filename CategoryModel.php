608763bf1148cb6dfa9df368127fcd98a2ccdf2d
ngocmai25 <133323346+ngocmai25@users.noreply.github.com>
2023-08-11 10:48:01 +0700

    update CategoryModel

---
+++ CategoryModel.php
@@ -0,0 +1,39 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL);
+$mysqli = new mysqli("localhost", "root", "", "dbtest");
+
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+    exit();
+}
+class CategoryModel
+{
+    private $mysqli;
+    
+    public function __construct($mysqli)
+    {
+        $this->mysqli = $mysqli;
+      
+    }
+    public function getCategory()
+    {
+        $sql_category_list = "SELECT * FROM category ORDER BY category_id DESC";
+        $query_category_list = mysqli_query($this->mysqli, $sql_category_list);
+
+        $categories = array();
+        while ($row = mysqli_fetch_array($query_category_list)) {
+            $categories[] = $row;
+        }
+        return $categories;
+        
+    }
+    // public function deleteCategory($categoryId)
+    // {
+    //     $sql_delete_category = "DELETE FROM category WHERE category_id = $categoryId";
+    //     mysqli_query($this->mysqli, $sql_delete_category);
+    // }
+}
+// $categoryModel = new CategoryModel($mysqli);
+
+// $categories = $categoryModel->getCategory();
