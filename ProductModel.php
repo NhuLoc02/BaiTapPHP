1f27b8619a9c074ce8bb4082573e7fc211526b5a
Nguyen Loc <133158446+NhuLoc02@users.noreply.github.com>
2023-08-12 04:34:42 +0700

    Update ProductModel.php

--- ProductModel.php
+++ ProductModel.php
@@ -1,62 +1,42 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('error_reporting', E_ALL);
-$mysqli = new mysqli("localhost", "root", "", "dbtest");
+$mysqli = new mysqli('localhost', 'root', '', 'test');
 
-if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
-    exit();
-}
-class ProductModel
-{
+
+
+class ProductModel {
     private $mysqli;
-    
-    public function __construct($mysqli)
-    {
-        $this->mysqli = $mysqli;
-      
-    }
-    public function getProduct()
-    {
-        $sql_product_list = "SELECT product.product_id, product.product_name, brand.brand_name, category.category_name, product.product_quantity, product.product_price_import, product.product_price, product.product_sale, product.product_description FROM product 
-        join brand on product.product_brand = brand.brand_id 
-        join category on product.product_category = category.category_id
-        ORDER BY product.product_id DESC";
-        $query_product_list = mysqli_query($this->mysqli, $sql_product_list);
 
+    public function __construct($mysqli) {
+        $this->mysqli = $mysqli;
+    }
+
+    public function getProduct() {
+        $sql_product_list = "SELECT product.product_id, product.product_name, product.product_image, evaluate.evaluate_rate, product.product_status, product.product_price, product.product_sale, product.product_price_import FROM product INNER JOIN evaluate on product.product_id = evaluate.product_id WHERE product.product_id LIMIT 5 ";
+
+        $query_product_list = $this->mysqli->query($sql_product_list);
         $products = array();
-        while ($row = mysqli_fetch_array($query_product_list)) {
+        
+        while ($row = $query_product_list->fetch_assoc()) {
+            // $query_evaluate_rating = "SELECT evaluate_rate FROM evaluate WHERE product_id='" . $row['product_id'] . "' AND evaluate_status = 1";
+            
+            // $query_product_list_rating = $this->mysqli->query($query_evaluate_rating);
+            // $products_rate = array();
+            // while ($row_rate = $query_product_list_rating -> fetch_assoc()){
+            //     $products_rate[] = $row_rate;
+            // }
+            // return $products_rate;
             $products[] = $row;
         }
+        
         return $products;
-        
+        }
+    public function getRowCount() {
+        $sql = "SELECT COUNT(*) FROM product";
+        $query = mysqli_query($this->mysqli, $sql);
+        $row = mysqli_fetch_array($query);
+        return $row[0];
     }
-    public function deleteProduct()
-    {
-        $sql_delete_product="delete from product where product_id = $product_id";
-        mysqli_query($this->mysqli, $sql_delete_product);
-    }
-    // public function editBrand($brandId, $brandName)
-    // {
-    //     $sql_edit_brand = "SELECT * FROM brand WHERE brand_id = '$brandId' LIMIT 1";
-    //     $query_edit_brand = $this->db->query($sql_edit_brand);
 
-    //     $brands = [];
-    //     while ($row = $query_edit_brand->fetch_assoc()) {
-    //         $brand = new BrandController($row['brand_id'], $row['brand_name']);
-    //         $brands[] = $brand;
-    //     }
-        
-    //     if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['brand_edit'])) {
-    //         $brandName = $_POST['brand_name'];
-    //         $sqlUpdate = "UPDATE brand SET brand_name='$brandName' WHERE brand_id='$brandId'";
-
-    //         $this->db->query($sqlUpdate);
-    //         header('Location: ../../index.php?action=brand&query=brand_list');
-    //         exit();
-    //     }
-// }
+    // Các phương thức hoạt động với cơ sở dữ liệu khác cho model sản phẩm
 }
-$productModel = new ProductModel($mysqli);
-
-$products = $productModel->getProduct();
+?>
