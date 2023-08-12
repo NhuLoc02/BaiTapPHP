7612c33ab906be7d19c01900f0fba5fb1ace5b6d
Nguyen Loc <133158446+NhuLoc02@users.noreply.github.com>
2023-08-12 15:24:27 +0700

    Update BrandController.php

--- BrandController.php
+++ BrandController.php
@@ -20,20 +20,28 @@ class BrandController
         $brands = $brandModel->getBrand();
         require_once './views/BrandListView.php';
     }
-
-
-    public function brandEdit($brandId, $brandName, $brandModel)
-    {
-        $brands = $brandModel->editBrand($brandId, $brandName);
-        require_once './views/BrandEditView.php';
-        // header('Location: ../../index.php?action=brand&query=brand_list');
-        exit();
+       
+    public function editBrand() {
+        global $brandModel;
+        if (isset($_GET['brand_id'])) {
+            $brandId = $_GET['brand_id'];
+            $data = $brandModel->getBrandById($brandId); // Truyền brandId vào phương thức getBrandById()
+    
+            if (isset($_POST['brand_edit'])) {
+                $brandName = $_POST['brand_name'];
+                $brandModel->editBrand($brandId, $brandName);
+                echo "<script>window.location.href = '../../demo/index.php?action=brand&query=brand_list';</script>";
+            }
+    
+            require_once './views/BrandEditView.php';
+        }
     }
-}
+        
+
 
 
 // ...
-$action = isset($_GET['action']) ? $_GET['action'] : null;
+}
 $brandId = isset($_GET['brand_id']) ? $_GET['brand_id'] : null; // Add this line to fetch the brand_id
 $brandName = isset($_POST['brand_name']) ? $_POST['brand_name'] : null;
-$brandController = new BrandController($brandId, '', $brandModel);
+$brandController = new BrandController($brandId, $brandName, $brandModel);
