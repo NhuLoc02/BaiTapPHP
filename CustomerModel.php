707a8b46e30145e0d24805eba639ab84ff970f00
ngocmai25 <133323346+ngocmai25@users.noreply.github.com>
2023-08-12 12:41:58 +0700

    CustomerModel.php

---
+++ CustomerModel.php
@@ -0,0 +1,49 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL);
+$mysqli = new mysqli("localhost", "root", "", "dbtest");
+
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+    exit();
+}
+
+class CustomerModel
+{
+    private $mysqli;
+    
+    public function __construct($mysqli)
+    {
+        $this->mysqli = $mysqli;
+    }
+    
+    public function getCustomer()
+    {
+        // if (isset($_GET['pagenumber'])) {
+        //     $page = $_GET['pagenumber'];
+        // } else {
+        //     $page = '1';
+        // }
+
+        // if ($page == '' || $page == 1) {
+        //     $begin = 0;
+        // } else {
+        //     $begin = ($page * 10) - 10;
+        // }
+
+        $sql_customer_list = "SELECT * FROM customer ORDER BY customer_id DESC";
+        $query_customer_list = mysqli_query($this->mysqli, $sql_customer_list);
+        $customers = array();
+        while ($row = mysqli_fetch_array($query_customer_list)) {
+            $customers[] = $row;
+        }
+        return $customers;
+    }
+    public function deleteCustomer($customerId)
+    {
+        $sql_delete_customer = "DELETE FROM customer WHERE customer_id = '$customerId'";
+        mysqli_query($this->mysqli, $sql_delete_customer);
+    }
+}
+
+?>
