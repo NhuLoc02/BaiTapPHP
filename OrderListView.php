3cedd7a29a89d4dfcac68b71d6513a0a5f72c2f5
ngocmai25 <133323346+ngocmai25@users.noreply.github.com>
2023-08-12 20:14:17 +0700

    Add files via upload

---
+++ OrderListView.php
@@ -0,0 +1,209 @@
+<div class="row">
+    <div class="col">
+        <div class="header__list d-flex space-between align-center">
+            <h3 class="card-title" style="margin: 0;">Danh sách đơn hàng online</h3>
+            <div class="action_group">
+                <a href="#" class="button button-dark">Export</a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-12 grid-margin stretch-card">
+        <div class="card">
+            <div class="card-body">
+                <div class="main-pane-top d-flex space-between align-center" style="padding-inline: 20px;">
+                    <div class="input__search p-relative">
+                        <form class="search-form" action="?action=order&query=order_search" method="POST">
+                            <i class="icon-search p-absolute"></i>
+                            <input type="search" name="order_search" class="form-control" placeholder="Search Here" title="Search here">
+                        </form>
+                    </div>
+                    <div class="dropdown dropdown__item">
+                        <button class="btn btn-outline-dark dropdown-toggle" type="button" id="dropdownMenuSizeButton2" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            
+                            <?php 
+                                if (isset($_GET['order_status']) && $_GET['order_status'] == 0) {
+                                    echo "Đơn đang xử lý";
+                                } elseif(isset($_GET['order_status']) && $_GET['order_status'] == 1) {
+                                    echo "Đang chuyển bị hàng";
+                                } elseif(isset($_GET['order_status']) && $_GET['order_status'] == 2) {
+                                    echo "Đang giao hàng";
+                                } elseif(isset($_GET['order_status']) && $_GET['order_status'] == 3) {
+                                    echo "Đã hoàn thành";
+                                } elseif(isset($_GET['order_status']) && $_GET['order_status'] == -1) {
+                                    echo "Đơn đã hủy";
+                                } else {
+                                    echo "Đang thực hiện";
+                                }
+                            ?>
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuSizeButton2">
+                            <a class="dropdown-item" href="index.php?action=order&query=order_list">Đơn đang thực hiện</a>
+                            <a class="dropdown-item" href="index.php?action=order&query=order_list&order_status=0">Đang xử lý</a>
+                            <a class="dropdown-item" href="index.php?action=order&query=order_list&order_status=1">Đang chuyển bị hàng</a>
+                            <a class="dropdown-item" href="index.php?action=order&query=order_list&order_status=2">Đang giao hàng</a>
+                            <a class="dropdown-item" href="index.php?action=order&query=order_list&order_status=3">Đã hoàn thành</a>
+                            <a class="dropdown-item" href="index.php?action=order&query=order_list&order_status=-1">Đã hủy</a>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="table-responsive">
+                    <table class="table table-hover table-action">
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>
+                                    <input type="checkbox" id="checkAll">
+                                </th>
+                                <th>Mã đơn hàng</th>
+                                <th>Thời gian</th>
+                                <th>Tên người đặt</th>
+                                <th>Loại đơn hàng</th>
+                                <th class="text-center">Tình trạng đơn hàng</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $i = 0;
+                            foreach ($orders as $row) {
+                                $i++;}
+                            ?>
+                                <tr>
+                                    <td>
+                                        <a href="?action=order&query=order_detail_online&order_code=<?php echo $row['order_code'] ?>">
+                                            <div class="icon-edit">
+                                                <img class="w-100 h-100" src="images/icon-view.png" alt="">
+                                            </div>
+                                        </a>
+                                    </td>
+                                    <td>
+                                        <input type="checkbox" class="checkbox" onclick="testChecked(); getCheckedCheckboxes();" id="<?php echo $row['order_code'] ?>">
+                                    </td>
+                                    <td><?php echo $row['order_code'] ?></td>
+                                    <td><?php echo $row['order_date'] ?></td>
+                                    <td><?php echo $row['account_name'] ?></td>
+                                    <td><?php echo format_order_type($row['order_type']); ?></td>
+                                    <td>
+                                        <?php if ($row['order_status'] == 0) {
+                                        ?>
+                                            <div class="order_status order_status--0">
+                                                <span class="show-status">Đơn đang xử lý</span>
+                                            </div>
+                                        <?php
+                                        } elseif ($row['order_status'] == 1) {
+                                        ?>
+                                            <div class="order__status order__status--1">
+                                                <span class="show-status">Đang chuẩn bị hàng</span>
+                                            </div>
+                                        <?php
+                                        } elseif ($row['order_status'] == 2) {
+                                        ?>
+                                             <div class="order__status order__status--2">
+                                                <span class="show-status">Đang giao hàng</span>
+                                            </div>
+                                        <?php
+                                        } elseif ($row['order_status'] == 3) {
+                                        ?>
+                                            <div class="order__status order__status--3">
+                                                <span class="show-status">Đã hoàn thành</span>
+                                            </div>
+                                        <?php
+                                        } elseif ($row['order_status'] == -1) {
+                                        ?>
+                                            <div class="order__status order__status---1">
+                                                <span class="show-status">Đơn đã hủy</span>
+                                            </div>
+                                            <?php
+                                             }
+                                            ?>
+                                        
+                                    </td>
+                                </tr>
+                            
+                        </tbody>
+                    </table>
+                </div>
+                
+<div class="dialog__control">
+    <div class="control__box">
+        <a href="modules/order/xuly.php?confirm=1" class="button__control" id="btnConfirm">Duyệt đơn hàng</a>
+        <a href="modules/order/xuly.php?cancel=1" class="button__control" id="btnCancel">Hủy đơn hàng</a>
+    </div>
+</div>
+<script>
+    var btnConfirm = document.getElementById("btnConfirm");
+    var btnCancel = document.getElementById("btnCancel");
+    var checkAll = document.getElementById("checkAll");
+    var checkboxes = document.getElementsByClassName("checkbox");
+    var dialogControl = document.querySelector('.dialog__control');
+    // Thêm sự kiện click cho checkbox checkAll
+    checkAll.addEventListener("click", function() {
+        // Nếu checkbox checkAll được chọn
+        if (checkAll.checked) {
+            // Đặt thuộc tính "checked" cho tất cả các checkbox còn lại
+            for (var i = 0; i < checkboxes.length; i++) {
+                checkboxes[i].checked = true;
+            }
+        } else {
+            // Bỏ thuộc tính "checked" cho tất cả các checkbox còn lại
+            for (var i = 0; i < checkboxes.length; i++) {
+                checkboxes[i].checked = false;
+            }
+        }
+        testChecked();
+        getCheckedCheckboxes();
+    });
+
+    function testChecked() {
+        var count = 0;
+        for (let i = 0; i < checkboxes.length; i++) {
+            if (checkboxes[i].checked) {
+                count++;
+                console.log(count);
+            }
+        }
+        if (count > 0) {
+            dialogControl.classList.add('active');
+        } else {
+            dialogControl.classList.remove('active');
+            checkAll.checked = false;
+        }
+    }
+
+    function getCheckedCheckboxes() {
+        var checkeds = document.querySelectorAll('.checkbox:checked');
+        var checkedIds = [];
+        for (var i = 0; i < checkeds.length; i++) {
+            checkedIds.push(checkeds[i].id);
+        }
+        btnConfirm.href = "modules/order/xuly.php?confirm=1&data=" + JSON.stringify(checkedIds);
+        btnCancel.href = "modules/order/xuly.php?cancel=1&data=" + JSON.stringify(checkedIds);
+    }
+</script>
+
+<script>
+    function showSuccessToast() {
+        toast({
+            title: "Success",
+            message: "Cập nhật thành công",
+            type: "success",
+            duration: 0,
+        });
+    }
+</script>
+
+<?php
+if (isset($_GET['message']) && $_GET['message'] == 'success') {
+    $message = $_GET['message'];
+    echo '<script>';
+    echo '   showSuccessToast();';
+    echo '</script>';
+}
+?>
+
+<script>
+    window.history.pushState(null, "", "index.php?action=order&query=order_list"+"<?php echo $url_status ?><?php echo $url_page ?>");
+</script>
