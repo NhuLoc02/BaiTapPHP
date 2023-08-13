b2806696d2d494be3af60e363b8e244f60d70a58
ngocmai25 <133323346+ngocmai25@users.noreply.github.com>
2023-08-13 15:07:31 +0700

    Update OrderModel.php

--- OrderModel.php
+++ OrderModel.php
@@ -1,61 +1,4 @@
 <?php
-// $mysqli = new mysqli('localhost', 'root', '', 'dbtest');
-
-// class OrderModel {
-//     private $mysqli;
-
-//     public function __construct($mysqli) {
-//         $this->mysqli = $mysqli;
-//     }
-
-//     public function getOrder() {
-//         if (isset($_GET['order_status'])) {
-//             $order_status = $_GET['order_status'];
-//             $sql_order_list = "SELECT * FROM orders JOIN account ON orders.account_id = account.account_id WHERE orders.order_status = ? ORDER BY orders.order_id DESC";
-//             $stmt = $this->mysqli->prepare($sql_order_list);
-//             $stmt->bind_param("i", $order_status);
-//             $stmt->execute();
-//             $query_order_list = $stmt->get_result();
-//         } else {
-//             $sql_order_list = "SELECT * FROM orders JOIN account ON orders.account_id = account.account_id WHERE orders.order_status >= 0 AND orders.order_status < 3 ORDER BY orders.order_id DESC";
-//             $query_order_list = mysqli_query($this->mysqli, $sql_order_list);
-//         }
-        
-//         $orders = array();
-        
-//         while ($row = mysqli_fetch_assoc($query_order_list)) {
-//             $orders[] = $row;
-//         }
-        
-//         return $orders;
-
-//         // if (isset($_GET['order_status'])) {
-//         //     $order_status = $_GET['order_status'];
-//         //     $sql_order_list = "SELECT * FROM orders JOIN account ON orders.account_id = account.account_id WHERE orders.order_status = $order_status ORDER BY orders.order_id DESC";
-//         //     $query_order_list = mysqli_query($this->mysqli, $sql_order_list);
-//         // } else {
-//         //     $sql_order_list = "SELECT * FROM orders JOIN account ON orders.account_id = account.account_id WHERE orders.order_status >= 0 AND orders.order_status < 3 ORDER BY orders.order_id DESC";
-//         //     $query_order_list = mysqli_query($this->mysqli, $sql_order_list);
-//         // }
-        
-//         // $orders = array();
-        
-//         // while ($row = mysqli_fetch_assoc($query_order_list)) {
-//         //     $orders[] = $row;
-//         // }
-        
-//         // return $orders;
-//     }
-
-//     public function getRowCount() {
-//         $sql = "SELECT COUNT(*) FROM orders";
-//         $query = mysqli_query($this->mysqli, $sql);
-//         $row = mysqli_fetch_array($query);
-//         return $row[0];
-//     }
-
-//     // Các phương thức hoạt động với cơ sở dữ liệu khác cho model sản phẩm
-// }
 
 $mysqli = new mysqli('localhost', 'root', '', 'dbtest');
 
@@ -67,7 +10,7 @@ class OrderModel {
     }
 
     public function getOrder() {
-        $sql_order_list = "SELECT * FROM orders JOIN account ON orders.account_id = account.account_id WHERE orders.order_status  ORDER BY orders.order_id LIMIT 3 ";
+        $sql_order_list = "SELECT * FROM orders JOIN account ON orders.account_id = account.account_id WHERE orders.order_status  ORDER BY orders.order_id  ";
         $query_order_list = $this->mysqli->query($sql_order_list);
 
        
@@ -86,6 +29,29 @@ class OrderModel {
         $row = mysqli_fetch_array($query);
         return $row[0];
     }
-    // Các phương thức hoạt động với cơ sở dữ liệu khác cho model sản phẩm
+
+    // public function getOrderDetail() {
+    //     $sql_order = "SELECT * FROM orders JOIN delivery ON orders.delivery_id = delivery.delivery_id WHERE orders.order_code  ORDER BY orders.order_id DESC";
+    //     mysqli_query($this->mysqli, $sql_order);
+        
+    // }
+    public function getOrderDetail($order_code) {
+        $sql = "SELECT * FROM orders WHERE order_code = '$order_code' LIMIT 1";
+        $query = mysqli_query($this->mysqli, $sql);
+        return mysqli_fetch_array($query);
+    }
+
+    public function getOrderDetails($order_code) {
+        $sql = "SELECT od.order_detail_id, p.product_id, p.product_name, od.product_quantity, od.product_price, od.product_sale, p.product_image FROM order_detail od JOIN product p ON od.product_id = p.product_id WHERE od.order_code = '$order_code' ORDER BY od.order_detail_id DESC";
+        $query = mysqli_query($this->mysqli, $sql);
+        return $query;
+    }
+
+    public function getOrderWithDelivery($order_code) {
+        $sql = "SELECT * FROM orders JOIN delivery ON orders.delivery_id = delivery.delivery_id WHERE orders.order_code = '$order_code' ORDER BY orders.order_id DESC";
+        $query = mysqli_query($this->mysqli, $sql);
+        return $query;
+    }
+
 }
 ?>
