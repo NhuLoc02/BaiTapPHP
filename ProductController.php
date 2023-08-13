b6fc2f2b4c37c0ee46b683f344bfe018ca62a3e6
Nguyen Loc <133158446+NhuLoc02@users.noreply.github.com>
2023-08-14 01:25:33 +0700

    Update ProductController.php

--- ProductController.php
+++ ProductController.php
@@ -1,13 +1,20 @@
 <?php
 require_once './models/ProductModel.php';
 $productModel = new ProductModel($mysqli);
-class ProductController {
-    public $ahihi;
-    private $model;
-
-    public function __construct() {
-        $this->model = new ProductModel($GLOBALS['mysqli']);
-        
+class ProductController 
+{
+    private $product_id;
+    private $brand_id;
+    private $category_id;
+    private $capacity_id;
+  
+    public function __construct($product_id = null,$brand_id = null,$category_id = null,$capacity_id = null) 
+    {
+    
+        $this ->product_id = $product_id;
+        $this ->brand_id = $brand_id;
+        $this ->category_id = $category_id;
+        $this ->capacity_id = $capacity_id;
     }
 
     public function productList($productModel) {
@@ -24,19 +31,24 @@ class ProductController {
         }
 
         $products = $productModel ->getProduct();
-        require_once './views/ProductListView.php';
-
-        
+        require_once './views/ProductListView.php';  
     }
-
-    // Các phương thức khác cho controller quản lý sản phẩm
-    public function deleteProduct($productId)
+    public function editProduct()
     {
-        $this->model->deleteProduct($productId);
-         echo "<script>window.location.href = '../../demo/demo/index.php?action=product&query=product_list';</script>";
-    }
-}
+        global $productModel;
+ 
+            $product_id = isset($_GET['product_id']) ? $_GET['product_id'] : null;
+            $brand_id = isset($_GET['brand_id']) ? $_GET['brand_id'] : null;
+            $category_id = isset($_GET['category_id']) ? $_GET['category_id'] : null;
+            $capacity_id = isset($_GET['capacity_id']) ? $_GET['capacity_id'] : null;
 
+            $products = $productModel->getProductId($product_id);
+            $brands = $productModel->getBrand($brand_id);
+            $category = $productModel->getCategory($category_id);
+            $capacity = $productModel->getCapacity($capacity_id);
+        require_once './views/ProductEditView.php';
+    }       
+}
 $productController = new ProductController();
 
 ?>
