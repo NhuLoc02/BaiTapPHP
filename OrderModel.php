b597127a346358467054e8cc5bcabb54e234d4dd
ngocmai25 <133323346+ngocmai25@users.noreply.github.com>
2023-08-14 13:08:57 +0700

    Update OrderModel.php

--- OrderModel.php
+++ OrderModel.php
@@ -10,16 +10,17 @@ class OrderModel {
     }
 
     public function getOrder() {
-        $mysqli = new mysqli('localhost', 'root', '', 'dbtest');
+
         if (isset($_GET['order_status'])) {
+           
             $order_status = $_GET['order_status'];
-        
-            $sql_order_list = "SELECT * FROM orders JOIN account ON orders.account_id = account.account_id WHERE orders.order_status = $order_status ORDER BY orders.order_id DESC LIMIT 10";
-            $query_order_list = mysqli_query($mysqli, $sql_order_list);
-        } else {
+            $sql_order_list = "SELECT * FROM orders JOIN account ON orders.account_id = account.account_id WHERE orders.order_status = '$order_status' ORDER BY orders.order_id DESC LIMIT 10";
+            $query_order_list = $this->mysqli->query($sql_order_list);
+        } 
+        else {
             
             $sql_order_list = "SELECT * FROM orders JOIN account ON orders.account_id = account.account_id WHERE orders.order_status >= 0 AND orders.order_status < 3 ORDER BY orders.order_id DESC LIMIT 10";
-            $query_order_list = mysqli_query($mysqli, $sql_order_list);
+            $query_order_list = $this->mysqli->query($sql_order_list);
         }
         $orders = array();
         
@@ -36,13 +37,57 @@ class OrderModel {
         $row = mysqli_fetch_array($query);
         return $row[0];
     }
+    public function getById() {
+        $sql_order_id = "SELECT * FROM orders WHERE order_code = '$_GET[order_code]' LIMIT 1 ";     
+        $query = $this->mysqli->query($sql_order_id);
+        $order = array();
+        while ($row = $query->fetch_assoc()){
+            $order[] = $row;
+        }     
+        return $order;   
+}
     public function getOrderDetail() {
-        $sql_order = "SELECT * FROM orders JOIN delivery ON orders.delivery_id = delivery.delivery_id WHERE orders.order_code  ORDER BY orders.order_id LIMIT 1 ";
-        $query_order_detail = mysqli_query($this->mysqli, $sql_order);
+        $sql_order_detail = "SELECT * FROM orders JOIN delivery ON orders.delivery_id = delivery.delivery_id WHERE orders.order_code = '$_GET[order_code]' ORDER BY orders.order_id DESC";
+        $query = $this->mysqli->query($sql_order_detail);
+        $orderDetails = array();
+        while ($row = $query->fetch_assoc()){
+            $orderDetails[] = $row;
+        }     
+        return $orderDetails;   
+}
+    public function getProductDetail() {
+        $sql_product_detail = "SELECT od.order_detail_id, p.product_id, p.product_name, od.product_quantity, od.product_price, od.product_sale, p.product_image FROM order_detail od JOIN product p ON od.product_id = p.product_id WHERE od.order_code = '$_GET[order_code]' ORDER BY od.order_detail_id DESC";
+        $query = $this->mysqli->query($sql_product_detail);
+        $productDetails = array();
+            while ($row = $query->fetch_assoc()){
+            $productDetails[] = $row;
+        }     
+        return $productDetails;   
+}
+//     public function confirmOrder() {
+//     $sql_confirm_order = "UPDATE orders SET order_status = '$_GET[order_status]' WHERE order_code = '$_GET[order_code]'";
+//     $query = $this->mysqli->query($sql_confirm_order);
+//     $getById = array();
+//         while ($row = $query->fetch_assoc()){
+//         $getById[] = $row;
+//     }     
+//     return $getById;
+// }
 
-        $orders = mysqli_fetch_assoc($query_order_detail);
-
-        return $orders;
+public function getOrderHistory() {
+    if (isset($_GET['payment_type']) && $_GET['payment_type']=='momo') {
+        $sql_order_history = "SELECT * FROM momo ORDER BY momo_id DESC LIMIT 5";
+    } else {
+        $sql_order_history = "SELECT * FROM vnpay ORDER BY vnp_paydate DESC LIMIT 5";
     }
-   
+    $query = $this->mysqli->query($sql_order_history);
+    $orderHistory = array();
+        while ($row = $query->fetch_assoc()){
+        $orderHistory[] = $row;
+    }     
+    return $orderHistory;
+}
+// other methods in the OrderModel class
+}
+
 ?>
