5d12cffad0815bf06aaf47d12096e04b915ceb5a
anhvan0210 <132814026+anhvan0210@users.noreply.github.com>
2023-08-17 00:35:50 +0700

    Create ContactView.php

---
+++ ContactView.php
@@ -0,0 +1,47 @@
+<section class="contact pd-top">
+    <div class="container">
+        <div class="contact__container">
+            <h1 class="contact__heading h2">Liên hệ</h1>
+            <h3 class="contact__title h4">Thời gian làm việc</h3>
+            <div class="contact__infomation">
+                <p>Thứ 2 – Thứ 6: 09:00 am – 09:30pm</p>
+                <p>Thứ 7: 10:00am – 11:00pm</p>
+                <p>Chủ nhật: 08:00am – 10:00pm</p>
+            </div>
+            <div class="contact__form pd-section">
+                <form action="" method="POST">
+                    <div class="row contact__input--double">
+                        <div class="col" style="--w-lg: 6">
+                            <div class="contact__input">
+                                <input class="w-100 btn" type="text" name="customer_name" placeholder="Tên khách hàng" />
+                            </div>
+                        </div>
+                        <div class="col" style="--w-lg: 6;">
+                            <div class="contact__input">
+                                <input class="w-100 btn" type="email" name="customer_email" placeholder="Email" />
+                            </div>
+                        </div>
+                        <div class="col" style="--w-lg: 12;">
+                            <div class="contact__input">
+                                <input class="w-100 btn" type="text" name="customer_phone" placeholder="Số điện thoại" />
+                            </div>
+                        </div>
+                        <div class="col" style="--w-lg: 12;">
+                            <div class="contact__textarea w-100 h-100">
+                                <textarea class="w-100 h-100 btn" name="customer_address" id="" cols="30" rows="10" placeholder="Địa chỉ"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <button class="btn contact__btn" name="customer_add" type="submit">
+                                Gửi
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- end contact -->
