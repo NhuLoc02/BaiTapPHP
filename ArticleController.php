bbd5301dbdd3ce166498ab9e486798e8cc9760ad
ngocmai25 <133323346+ngocmai25@users.noreply.github.com>
2023-08-17 01:40:01 +0700

    Create ArticleController.php

---
+++ ArticleController.php
@@ -0,0 +1,29 @@
+<?php
+require './pages/models/ArticleModel.php';
+require './pages/views/ArticleView.php';
+class ArticleController {
+    private $model;
+    private $view;
+
+    public function __construct($model, $view) {
+        $this->model = $model;
+        $this->view = $view;
+    }
+
+    public function showArticle($article_id) {
+        $article = $this->model->getArticle($article_id);
+        $comments = $this->model->getComments($article_id);
+        $comment_total = mysqli_num_rows($comments);
+
+        $this->view->renderArticle($article);
+        $this->view->renderComments($comments, $comment_total);
+    }
+
+    public function addComment($article_id, $comment_name, $comment_email, $comment_content) {
+        // Handle comment submission logic here
+    }
+}
+$model = new ArticleModel($mysqli);
+$view = new ArticleView();
+$controller = new ArticleController($model, $view);
+?>
