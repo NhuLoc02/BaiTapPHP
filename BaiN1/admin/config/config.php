4abbd69da5a48d585515d1e984e28b6553a4df58
Nguyen Loc <133158446+NhuLoc02@users.noreply.github.com>
2023-08-17 17:31:41 +0700

    Delete BaiN1/admin directory

--- BaiN1/admin/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $mysqli = new mysqli("localhost:3306","root","","test");
-
-    // Check connection
-    if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
-    exit();
-}
